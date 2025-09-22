ce2668d4038cfd69ce1f0be9d559a582602b7219
venkataraviduddugunta7 <venkataraviduddugunta@gmail.com>
2025-09-23 01:47:26 +0530

    added the children style

--- components/layout/AppLayout.jsx
+++ components/layout/AppLayout.jsx
@@ -30,7 +30,7 @@ export default function AppLayout({ children }) {
             <div className="sticky top-0 z-40">
               <Header />
             </div>
-            <main className="flex-1 overflow-y-auto p-6">
+            <main className="flex-1 overflow-y-auto">
               {children}
             </main>
           </div>
